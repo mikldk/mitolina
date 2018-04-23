04f0fa6add1e7f142c542522dafb601ba11246fa
Mikkel Meyer Andersen <mikl@mikl.dk>
2018-04-23 20:42:30 +1200

    imports

--- R/mitolina-package.R
+++ R/mitolina-package.R
@@ -1,2 +1,7 @@
 #' @useDynLib mitolina
+#' @importFrom igraph graph_from_data_frame plot.igraph union layout_as_tree layout.reingold.tilford vcount V
+#' @import tibble
+#' @importFrom graphics par
+#' @importFrom utils head
+#' @importFrom methods is
 NULL
